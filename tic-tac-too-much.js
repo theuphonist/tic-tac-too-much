caae1904f0a150ed121401c06dd59f4a5a15d20a
Josh <mack8.jo@gmail.com>
2024-01-04 22:27:18 -0500

    add symbol entry and improve win/draw checking

--- tic-tac-too-much.js
+++ tic-tac-too-much.js
@@ -1,25 +1,50 @@
 const cells = Array.from(document.getElementsByClassName("cell"));
-const playerSymbols = ["X", "O"];
+const symbolInputGroup = document
+  .getElementsByClassName("symbol-input-group")
+  .item(0);
+const symbolInputs = Array.from(
+  document.getElementsByClassName("symbol-input")
+);
+const playButton = document.getElementById("play-button");
+
+const playerSymbols = [];
 const playerStatus = [0, 0];
 const winConditions = [
   0b111000000, 0b000111000, 0b000000111, 0b100100100, 0b010010010, 0b001001001,
   0b100010001, 0b001010100,
 ];
 
+let gameInProgress = false;
 let currentPlayer = 0;
 
 cells.forEach((cell, position) => {
   cell.addEventListener("click", () => {
-    if (cell.innerText === "") {
+    if (cell.innerText === "" && gameInProgress) {
       cell.innerText = playerSymbols[currentPlayer];
       playerStatus[currentPlayer] |= 1 << position;
 
       if (checkWin(currentPlayer)) {
+        gameInProgress = false;
+
         // use setTimeout to delay alert until after current player symbol displays on the board
         setTimeout(() => {
           alert(`${playerSymbols[currentPlayer]} Wins!`);
           clearBoard();
         }, 0);
+
+        showElement(symbolInputGroup);
+
+        [playerStatus[0], playerStatus[1]] = [0, 0];
+      } else if (checkDraw()) {
+        gameInProgress = false;
+
+        // use setTimeout to delay alert until after current player symbol displays on the board
+        setTimeout(() => {
+          alert(`It's a draw!`);
+          clearBoard();
+        }, 0);
+
+        showElement(symbolInputGroup);
 
         [playerStatus[0], playerStatus[1]] = [0, 0];
       } else {
@@ -29,10 +54,29 @@ cells.forEach((cell, position) => {
   });
 });
 
+playButton.addEventListener("click", () => {
+  if (symbolInputs[0].value.trim() === symbolInputs[1].value.trim()) {
+    alert("Players must have different symbols!");
+  } else if (symbolInputs.some((symbol) => symbol.value.trim() === "")) {
+    alert("Player symbol cannot be empty!");
+  } else if (!gameInProgress) {
+    [playerSymbols[0], playerSymbols[1]] = [
+      symbolInputs[0].value,
+      symbolInputs[1].value,
+    ];
+    hideElement(symbolInputGroup);
+    gameInProgress = true;
+  }
+});
+
 function checkWin(player) {
   return winConditions.some(
     (condition) => (playerStatus[player] & condition) === condition
   );
+}
+
+function checkDraw() {
+  return (playerStatus[0] | playerStatus[1]) === 0b111111111;
 }
 
 function getNextPlayer(currentPlayer) {
@@ -41,4 +85,12 @@ function getNextPlayer(currentPlayer) {
 
 function clearBoard() {
   cells.forEach((cell) => (cell.innerText = ""));
+}
+
+function hideElement(element) {
+  element.style.display = "none";
+}
+
+function showElement(element, style = "block") {
+  element.style.display = style;
 }
