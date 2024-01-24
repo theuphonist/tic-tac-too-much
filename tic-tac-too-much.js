0cbd09b3b18cb75a40e41a6351eee4ffd84c5e34
Josh <mack8.jo@gmail.com>
2024-01-23 21:59:42 -0500

    add colors for shroud and logic for determining winner of overall game

--- tic-tac-too-much.js
+++ tic-tac-too-much.js
@@ -1,6 +1,5 @@
 /* TO DO:
-    - Add logic to determine when a full game is over and show the setup block again
-    - Add logic to determine who wins the overall game by counting who won each board
+    - Add logic to show winner and a "play again" button
     - Add logic for turn timer on each board
     - Add logic for symbol input that grays out background and gives setup options
     - Make everything look nicer
@@ -12,21 +11,30 @@ const winConditions = [
 ];
 
 class Player {
-  constructor(symbol) {
+  constructor(symbol, color) {
     this.symbol = symbol;
+    this.color = color;
   }
 
   get symbol() {
     return this._symbol;
   }
 
-  set symbol(symbol) {
-    this._symbol = symbol;
+  set symbol(sym) {
+    this._symbol = sym;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(col) {
+    this._color = col;
   }
 }
 
 class TicTacToeBoard {
-  constructor(isNewHTML, ...playerSymbols) {
+  constructor(isNewHTML, parentGame) {
     // only create new HTML elements if this is a new board
     if (isNewHTML) {
       this.HTMLElement = document.querySelector(".board-area").cloneNode(true);
@@ -39,12 +47,18 @@ class TicTacToeBoard {
     this.cells.forEach((cell, position) => {
       cell.addEventListener("click", () => {
         if (cell.innerText === "" && this.isInProgress) {
-          cell.innerText = this.playerSymbols[this.currentPlayer];
+          cell.innerText = this.parentGame.players[this.currentPlayer].symbol;
           this.playerStatuses[this.currentPlayer] |= 1 << position;
 
           if (this.checkWin()) {
             this.isInProgress = false;
-            this.showShroud(this.playerSymbols[this.currentPlayer] + " wins!");
+            this.showShroud(
+              this.parentGame.players[this.currentPlayer].symbol + " wins!",
+              this.parentGame.players[this.currentPlayer].color
+            );
+
+            this.parentGame.addWin(this.currentPlayer);
+            this.parentGame.checkOverallWin();
 
             // // use setTimeout to delay alert until after current player symbol displays on the board
             // setTimeout(() => {
@@ -56,7 +70,10 @@ class TicTacToeBoard {
             [this.playerStatuses[0], this.playerStatuses[1]] = [0, 0];
           } else if (this.checkDraw()) {
             this.isInProgress = false;
-            this.showShroud("It's a draw!");
+            this.showShroud("It's a draw!", "rgba(200, 200, 200, 0.9)");
+
+            this.parentGame.addDraw(this.currentPlayer);
+            this.parentGame.checkOverallWin();
 
             // // use setTimeout to delay alert until after current player symbol displays on the board
             // setTimeout(() => {
@@ -76,7 +93,7 @@ class TicTacToeBoard {
     });
 
     this.playerStatuses = [0, 0];
-    this.playerSymbols = playerSymbols;
+    this.parentGame = parentGame;
     this.isInProgress = false;
     this.currentPlayer = 0;
   }
@@ -110,17 +127,21 @@ class TicTacToeBoard {
     this.HTMLElement.querySelector(".board-area div").style.display = "none";
   }
 
-  showShroud(text) {
+  showShroud(text, color) {
     let shroudElement = this.HTMLElement.querySelector(".board-area div");
     shroudElement.style.display = "flex";
+    shroudElement.style.backgroundColor = color;
     shroudElement.innerText = text;
   }
 }
 
 class TicTacToeGame {
   constructor() {
-    this.players = [new Player("X"), new Player("O")];
-    this.boards = [new TicTacToeBoard(false, "", "")];
+    this.players = [
+      new Player("X", "rgba(77, 148, 255, 0.9)"),
+      new Player("O", "rgba(255, 172, 47, 0.9)"),
+    ];
+    this.boards = [new TicTacToeBoard(false, this)];
     this.maxBoardCount = 20;
 
     this.setupBlock = document.querySelector("#setup-block");
@@ -146,7 +167,7 @@ class TicTacToeGame {
 
         if (boardCount > this.boards.length) {
           for (let i = this.boards.length; i < boardCount; i++) {
-            this.boards.push(new TicTacToeBoard(true, "", ""));
+            this.boards.push(new TicTacToeBoard(true, this));
           }
         } else if (boardCount < this.boards.length && boardCount >= 1) {
           this.boards
@@ -155,6 +176,9 @@ class TicTacToeGame {
         }
       }
     });
+
+    this.wins = [0, 0];
+    this.draws = 0;
   }
 
   hideSetup() {
@@ -171,7 +195,6 @@ class TicTacToeGame {
       this.players[1].symbol = this.symbolInputs[1].value;
 
       this.boards.forEach((board) => {
-        board.playerSymbols = [this.players[0].symbol, this.players[1].symbol];
         board.isInProgress = true;
         board.clearBoard();
         board.hideShroud();
@@ -194,88 +217,23 @@ class TicTacToeGame {
 
     return false;
   }
+
+  addWin(playerIndex) {
+    this.wins[playerIndex]++;
+  }
+
+  addDraw() {
+    this.draws++;
+  }
+
+  checkOverallWin() {
+    let availableWins = this.boards.length - this.draws;
+    if (this.wins[0] > availableWins / 2) {
+      console.log("Player 1 wins!");
+    } else if (this.wins[1] > availableWins / 2) {
+      console.log("Player 2 wins!");
+    }
+  }
 }
 
 const game = new TicTacToeGame();
-document.getElementById("TEST-BUTTON").addEventListener("click", () => {
-  document.querySelector(".board-area div").style.display = "none";
-});
-// const cells = Array.from(document.getElementsByClassName("cell"));
-// const symbolInputGroup = document
-//   .getElementsByClassName("symbol-input-group")
-//   .item(0);
-// const symbolInputs = Array.from(
-//   document.getElementsByClassName("symbol-input")
-// );
-// const playButton = document.getElementById("play-button");
-
-// const playerSymbols = [];
-// const playerStatus = [0, 0];
-// const winConditions = [
-//   0b111000000, 0b000111000, 0b000000111, 0b100100100, 0b010010010, 0b001001001,
-//   0b100010001, 0b001010100,
-// ];
-
-// let gameInProgress = false;
-// let currentPlayer = 0;
-
-// cells.forEach((cell, position) => {
-//   cell.addEventListener("click", () => {
-//     if (cell.innerText === "" && gameInProgress) {
-//       cell.innerText = playerSymbols[currentPlayer];
-//       playerStatus[currentPlayer] |= 1 << position;
-
-//       if (checkWin(currentPlayer)) {
-//         gameInProgress = false;
-
-//         // use setTimeout to delay alert until after current player symbol displays on the board
-//         setTimeout(() => {
-//           alert(`${playerSymbols[currentPlayer]} Wins!`);
-//           clearBoard();
-//         }, 0);
-
-//         showElement(symbolInputGroup);
-
-//         [playerStatus[0], playerStatus[1]] = [0, 0];
-//       } else if (checkDraw()) {
-//         gameInProgress = false;
-
-//         // use setTimeout to delay alert until after current player symbol displays on the board
-//         setTimeout(() => {
-//           alert(`It's a draw!`);
-//           clearBoard();
-//         }, 0);
-
-//         showElement(symbolInputGroup);
-
-//         [playerStatus[0], playerStatus[1]] = [0, 0];
-//       } else {
-//         currentPlayer = getNextPlayer(currentPlayer);
-//       }
-//     }
-//   });
-// });
-
-// playButton.addEventListener("click", () => {
-//   // use trim to remove whitespace in symbol names before comparing
-//   if (symbolInputs[0].value.trim() === symbolInputs[1].value.trim()) {
-//     alert("Players must have different symbols!");
-//   } else if (symbolInputs.some((symbol) => symbol.value.trim() === "")) {
-//     alert("Player symbol cannot be empty!");
-//   } else if (!gameInProgress) {
-//     [playerSymbols[0], playerSymbols[1]] = [
-//       symbolInputs[0].value,
-//       symbolInputs[1].value,
-//     ];
-//     hideElement(symbolInputGroup);
-//     gameInProgress = true;
-//   }
-// });
-
-// function hideElement(element) {
-//   element.style.display = "none";
-// }
-
-// function showElement(element, style = "block") {
-//   element.style.display = style;
-// }
